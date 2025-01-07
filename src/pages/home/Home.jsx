2005fdcf39c7f29900f43e0caa2f293a29e6e3f1
RamziddinCoder <mrramziddindeveloper@gmail.com>
2025-01-07 20:10:49 +0500

    add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,82 +1,18 @@
-// import React from "react";
-// import "./home.scss";
-// import CreditCardIcon from "@mui/icons-material/CreditCard";
-// import CastForEducationIcon from "@mui/icons-material/CastForEducation";
-// import PersonIcon from "@mui/icons-material/Person";
-// import GroupsIcon from "@mui/icons-material/Groups";
-// import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
-// import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
-
-// const Home = () => {
-//   return (
-//     <div className="home">
-//       <h1>Bosh sahifa</h1>
-//       <div className="home__cards">
-//         <div className="home__card">
-//           <div className="home__card__icons">
-//             <CreditCardIcon />
-//             <h4>Jami</h4>
-//           </div>
-//           <div className="home__card__info">500</div>
-//         </div>
-
-//         <div className="home__card">
-//           <div className="home__card__icons">
-//             <PersonIcon />
-//             <h4>Faol O'quvchilar</h4>
-//           </div>
-//           <div className="home__card__info">300</div>
-//         </div>
-
-//         <div className="home__card">
-//           <div className="home__card__icons">
-//             <GroupsIcon />
-//             <h4>Guruhlar</h4>
-//           </div>
-//           <div className="home__card__info">20</div>
-//         </div>
-
-//         <div className="home__card">
-//           <div className="home__card__icons">
-//             <ErrorOutlineIcon />
-//             <h4>Qarzdorlar</h4>
-//           </div>
-//           <div className="home__card__info">10</div>
-//         </div>
-//       </div>
-//       <div>
-//         <div>
-//           <Gauge
-//             value={75}
-//             startAngle={-110}
-//             endAngle={110}
-//             sx={{
-//               [`& .${gaugeClasses.valueText}`]: {
-//                 fontSize: 40,
-//                 transform: "translate(0px, 0px)",
-//               },
-//             }}
-//             text={({ value, valueMax }) => `${value} / ${valueMax}`}
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
 import React from "react";
 import "./home.scss";
 import CreditCardIcon from "@mui/icons-material/CreditCard";
 import PersonIcon from "@mui/icons-material/Person";
 import GroupsIcon from "@mui/icons-material/Groups";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
-import { Gauge, gaugeClasses } from "@mui/x-charts";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { LineChart } from "@mui/x-charts/LineChart";
 
 const Home = () => {
   return (
     <div className="home">
       <h1>Bosh sahifa</h1>
+
       <div className="home__cards">
         <div className="home__card">
           <div className="home__card__icons">
@@ -111,23 +47,38 @@ const Home = () => {
         </div>
       </div>
 
-      {/* Gauge Diagramma */}
       <div className="home__gauge">
-        <Gauge
-          value={75}
-          valueMax={100}
-          startAngle={-110}
-          endAngle={110}
-          thickness={20}
-          sx={{
-            [`& .${gaugeClasses.valueText}`]: {
-              fontSize: "1.5rem",
-              fill: "#555",
-            },
-          }}
-          text={({ value, valueMax }) => `${value}%`}
-          color="primary"
-        />
+        <h2>Progress</h2>
+        <div className="home__gauge__card">
+          <div className="home__gauge__card-left">
+            <CircularProgressbar
+              value={75}
+              maxValue={100}
+              text={`75%`}
+              styles={buildStyles({
+                textSize: "16px",
+                pathColor: `#007bff`,
+                textColor: "#555",
+                trailColor: "#d6d6d6",
+              })}
+            />
+            <h3 className="home__gauge__card-left-text">
+              O'quvchilar davomati
+            </h3>
+          </div>
+          <div className="home__gauge__card-right">
+            <LineChart
+              xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+              series={[
+                {
+                  data: [2, 5.5, 2, 8.5, 1.5, 5],
+                  area: true,
+                },
+              ]}
+            />
+            <h3 className="home__gauge__card-right-title">To'lovlar tarixi</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
